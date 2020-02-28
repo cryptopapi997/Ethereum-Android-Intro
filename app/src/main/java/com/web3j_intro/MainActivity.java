f39d9fcd9bc366045923abdabadd1d341afb71e8
Nicolas Schapeler <nicoschap@live.fr>
2020-02-28 18:31:37 +0100

    fixed connection and wallet loading bugs

--- app/src/main/java/com/web3j_intro/MainActivity.java
+++ app/src/main/java/com/web3j_intro/MainActivity.java
@@ -31,8 +31,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupBouncyCastle();
         walletPath = getFilesDir().getAbsolutePath();
-        walletDir = new File(walletPath);
 
     }
 
@@ -56,7 +56,8 @@ public class MainActivity extends AppCompatActivity {
     public void createWallet(View v){
 
         try{
-            WalletUtils.generateNewWalletFile(password, walletDir);
+            fileName =  WalletUtils.generateLightNewWalletFile(password,walletDir);
+            walletDir = new File(walletPath + "/" + fileName);
             toastAsync("Wallet generated");
         }
         catch (Exception e){
@@ -90,5 +91,24 @@ public class MainActivity extends AppCompatActivity {
         runOnUiThread(() -> {
             Toast.makeText(this, message, Toast.LENGTH_LONG).show();
         });
+    }
+
+    // Workaround for bug with ECDA signatures.
+    private void setupBouncyCastle() {
+      final Provider provider = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME);
+      if (provider == null) {
+        // Web3j will set up the provider lazily when it's first used.
+        return;
+      }
+      if (provider.getClass().equals(BouncyCastleProvider.class)) {
+        // BC with same package name, shouldn't happen in real life.
+        return;
+      }
+      // Android registers its own BC provider. As it might be outdated and might not include
+      // all needed ciphers, we substitute it with a known BC bundled in the app.
+      // Android's BC has its package rewritten to "com.android.org.bouncycastle" and because
+      // of that it's possible to have another BC implementation loaded in VM.
+      Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME);
+      Security.insertProviderAt(new BouncyCastleProvider(), 1);
     }
 }
